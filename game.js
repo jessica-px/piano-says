818171c9e9899ff44dff82530b798857cd4e52a8
Jessica Peck <jessypeck@gmail.com>
2018-03-21 18:32:51 -0700

    Fixed seq length again

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ export {getPlayerInput};
 import {computer} from "./computer.js";
 
 var gameFinished = false;
-var seqLength = 1;
+var seqLength = 20;
 var turnNum = 0;
 var playerInput = [];
 var synth = new Tone.Synth().toMaster();
