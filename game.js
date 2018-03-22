d23ce346e8e9c37965cb994e1ff8b8749beb2e70
Jessica Peck <jessypeck@gmail.com>
2018-03-21 18:31:42 -0700

    Added Win/Lose tunes

--- game.js
+++ game.js
@@ -3,9 +3,10 @@ export {getPlayerInput};
 import {computer} from "./computer.js";
 
 var gameFinished = false;
-var seqLength = 20;
+var seqLength = 1;
 var turnNum = 0;
 var playerInput = [];
+var synth = new Tone.Synth().toMaster();
 
 export var game = {
     begin : begin,
@@ -77,10 +78,21 @@ function playerSuccess(){
 function win(){
     gameFinished = true;
     game.messageDiv.innerHTML = "You win! Press <i class='fas fa-redo-alt fa-xs'></i> to play again.";
+    playTune([["c5", "c5", "c5"], ["a4", "b4"], "c5"]);
 }
 
 function lose(){
     gameFinished = true;
     game.messageDiv.innerHTML = "You lost. Press <i class='fas fa-redo-alt fa-xs'></i> to try again.";
+    playTune([["a3", "a3", "a3"], ["g3", "d3"], "c#3"]);
+}
 
+function playTune(notes){
+    Tone.Transport.stop();
+    var thisTune = new Tone.Sequence(function(time,note){
+        synth.triggerAttackRelease(note, "16n", time);
+    }, notes).start(0);
+    thisTune.loop = false;
+    Tone.Transport.bpm.value = 150;
+    Tone.Transport.start("+0.1");
 }
